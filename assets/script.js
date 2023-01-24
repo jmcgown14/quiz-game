ded1d8e21cd909ef07e83d15ba0643abd1a137ab
Johnny McGown <jmac3292@gmail.com>
2023-01-24 15:03:08 -0700

    worked with tutor on fixing error of questions and answers not showing properly as well as local storage

--- assets/script.js
+++ assets/script.js
@@ -3,76 +3,77 @@ var onScreenTimer
 var startButton = document.querySelector(".start-button")
 var correct = document.querySelector(".correct")
 var incorrect = document.querySelector(".incorrect")
+var mainContainer = document.querySelector('.main')
 
-var allQuestion = {
-    'What symbol is used to comment in javascript?': ['?', '/', '!', 1],
-    'What is it called when you pre-write your code?': ['Psuedo Coding', 'Pre-Coding', 'Spider-Webbing', 1],
-    'What does this === mean?': ['Equal', 'Strictly Equal', 'Same As', 1],
-//     question: 'What symbol is used to comment in javascript?',
-//     choices: {
-//         incorrect: ['?', '!'],
-//         correct: '/',
-//     }
-// }, {
-//     question: 'What is it called when you pre-write your code?',
-//     choices: {
-//         incorrect: ['Pre-Coding', 'Spider-Webbing'],
-//         correct: 'Psuedo Coding',
-//     }
-// }, {
-//     question: 'What does this === mean?',
-//     choices: ['Similar', 'Not Equal', 'Strictly Equal'],
-//     }
-}
+var allQuestions = {
+    'What symbol is used to comment in javascript?': ['?', '/', '!', '?'],
+    'What is it called when you pre-write your code?': ['Psuedo Coding', 'Pre-Coding', 'Spider-Webbing', 'Psuedo Coding'],
+    'What does this === mean?': ['Equal', 'Strictly Equal', 'Same As', 'Strictly Equal'],
+};
 
-var timerCount = document.querySelector("timer-count")
+var time = 200
+var score = 0
+var timerCount = document.querySelector(".timer-count")
 var currentQuestion = 0
 var answer1Btn = document.querySelector("#answer1")
 var answer2Btn = document.querySelector("#answer2")
 var answer3Btn = document.querySelector("#answer3")
 
-
-// Start Game Function
-startButton.addEventListener("click", askQuestion);
-
-
 // Timer function for game
-// function startTimer() {
-//     // Sets timer
-//     timer = setInterval(function () {
-//         timerCount--;
-//         onScreenTimer.textContent = timerCount;
-//         if (timerCount >= 0) {
-//             // Tests if win condition is met
-//             if (isWin && timerCount > 0) {
-//                 // Clears interval and stops timer
-//                 clearInterval(timer);
-//                 winGame();
-//             }
-//         }
-//         // Tests if time has run out
-//         if (timerCount === 0) {
-//             // Clears interval
-//             clearInterval(timer);
-//             loseGame();
-//         }
-//     }, 4000);
-// }
+function startTimer() {
+    timerCount.textContent = time
+    // Sets timer
+    var timer = setInterval(function () {
+        time--;
+        timerCount.textContent = time;
+        if (time === 0 || currentQuestion > Object.keys(allQuestions).length - 1) {
+            // Tests if win condition is met
+            clearInterval(timer)
+            endQuiz()
+        }
+    }, 1000);
+}
 
+function endQuiz() {
+    mainContainer.textContent = ''
+    var inputEl = document.createElement('input')
+    inputEl.setAttribute('placeholder', 'Name')
 
+    var submitBtn = document.createElement('button')
+    submitBtn.textContent = "SUBMIT"
+
+    mainContainer.append(inputEl, submitBtn)
+
+    submitBtn.addEventListener('click', function() {
+        var  userData = {
+            name: inputEl.value,
+            finalScore: score
+        }
+
+        var storage = JSON.parse(localStorage.getItem('quizScores'))
+        if(storage === null) {
+            storage = []
+        }
+
+        storage.push(userData)
+        localStorage.setItem('quizScores', JSON.stringify(storage))
+        
+        window.location.href = 'highscore.html'
+    })
+}
 
 // Function to ask questions
 function askQuestion() {
-    var question = object.keys(allQuestion)[currentQuestion];
-    var answers = object.keys(allQuestion)[currentQuestion];
-    var answer1Text = answers[0]
-    var answer2Text = answers[1]
-    var answer3Text = answers[2]
 
-    if (currentQuestion==0) {
-        window.setTimeout(timeExpires, 40000);
-    }
-    var questionInfo= document.getElementsByClassName("questionInfo");
+    if (currentQuestion > Object.keys(allQuestions).length - 1) return
+    var question = Object.keys(allQuestions)[currentQuestion];
+    var answers = Object.keys(allQuestions)[currentQuestion];
+
+    var answer1Text = allQuestions[answers][0];
+    var answer2Text = allQuestions[answers][1];
+    var answer3Text = allQuestions[answers][2];
+
+    var questionInfo = document.querySelector(".questionInfo");
     questionInfo.innerHTML = question;
     answer1Btn.innerHTML = answer1Text;
     answer2Btn.innerHTML = answer2Text;
@@ -81,54 +82,54 @@ function askQuestion() {
 
 function answerQuestions() {
     currentQuestion++;
+    askQuestion();
+}
+
+function selectAnswer1(event) {
+    var answers = Object.values(allQuestions)[currentQuestion];
+    var correctAnswer = answers[3];
+    var correct = false;
+    if (correctAnswer === event.target.textContent) {
+        correct = true;
+        score += 33
+    }
+    alert('correct ' + correct + "score: " + score);
+    answerQuestions();
+}
+
+function selectAnswer2(event) {
+    var answers = Object.values(allQuestions)[currentQuestion];
+    var correctAnswer = answers[3];
+    var correct = false;
+    if (correctAnswer === event.target.textContent) {
+        correct = true;
+        score += 33
+    }
+    alert('correct ' + correct+ "score: " + score);
+    answerQuestions();
+}
+
+function selectAnswer3(event) {
+    var answers = Object.values(allQuestions)[currentQuestion];
+    var correctAnswer = answers[3];
+    var correct = false;
+    if (correctAnswer === event.target.textContent) {
+        correct = true;
+        score += 33
+    }
+    alert('correct ' + correct+ "score: " + score);
+    answerQuestions();
+}
+
+// function timeExpires (){
+//     alert('time expires');
+// }
+
+startButton.addEventListener("click", function () {
+    startTimer()
     askQuestion()
-}
+});
 
-function selectAnswer1 (){
-    var answers = Object.values(allQuestions)[currentQuestion];
-    var correctAnswer = answers[1];
-    var correct = false;
-    if (correctAnswer == 0) {
-        correct = true;
-    }
-    alert('correct '+ correct);
-    answerQuestions();
-}
-
-function selectAnswer2 (){
-    var answers = Object.values(allQuestions)[currentQuestion];
-    var correctAnswer = answers[1];
-    var correct = false;
-    if (correctAnswer == 0) {
-        correct = true;
-    }
-    alert('correct '+ correct);
-    answerQuestions();
-}
-
-function selectAnswer3 (){
-    var answers = Object.values(allQuestions)[currentQuestion];
-    var correctAnswer = answers[1];
-    var correct = false;
-    if (correctAnswer == 0) {
-        correct = true;
-    }
-    alert('correct '+ correct);
-    answerQuestions();
-}
-
-
-
-
-
-
-
-answer1Button.addEventListener("click", selectAnswer1);
-answer2Button.addEventListener("click", selectAnswer2);
-answer3Button.addEventListener("click", selectAnswer3);
-answer4Button.addEventListener("click", selectAnswer4);
-// Init function to store wins
-
-
-
-//local stoagre to store quiz scores
+answer1Btn.addEventListener("click", (event) => selectAnswer1(event));
+answer2Btn.addEventListener("click", (event) => selectAnswer2(event));
+answer3Btn.addEventListener("click", (event) => selectAnswer3(event));
